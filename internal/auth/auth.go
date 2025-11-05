c22bfc65528277e6481bac0f5427359610c23398
FrogOnABike <mark.skrzypczyk.com>
2025-11-05 22:18:24 +0000

    Ch6L6.1-2

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,10 +2,13 @@ package auth
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
 
 func HashPassword(password string) (string, error) {
@@ -25,5 +28,26 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 }
 
 func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
-	// Implementation for creating JWT
+	// Load environment variables
+	godotenv.Load()
+
+	// Get the JWT secret key from environment variables
+	mySigningKey := []byte(os.Getenv("JWT_SECRET"))
+
+	// Create the JWT claims, which includes the user ID and expiry time
+	claims := &jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		Subject:   userID.String(),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+	}
+	// Create the token using the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign the token with the secret key
+	signedToken, err := token.SignedString(mySigningKey)
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
 }
